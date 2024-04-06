36c388e70050cdac8931f110288cd1d6480bc843
karthee21 <mutyalakarthik6@gmail.com>
2024-04-07 00:09:32 +0530

    big-discount,new-arrivals part completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,9 +3,11 @@ import './Home.css'
 
 
 // -----------------------------------
-import SliderData from '../Assets/products'
+import SliderData, { discoutProducts, products } from '../Assets/products'
 import { serviceData } from '../Assets/products'
-  
+import { Plus } from 'react-feather'
+import { LiaStarSolid } from "react-icons/lia";
+
 function Home() {
     return (
         <>
@@ -27,7 +29,7 @@ function Home() {
                         ))
                     ) :
                         <div class="spinner-grow text-info" role="status">
-                            <span class="visually-hidden">Loading...</span>
+                            <span class="">Loading...</span>
                         </div>
                     }
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
@@ -41,287 +43,78 @@ function Home() {
                 </div>
             </div>
             <div className='service'>
-            {serviceData.length > 0 ? (
-            serviceData.map((service, index) => (
-                 <div key={index} style={{background:service.bg}}>
-                    <h2>{service.icon}</h2>
-                    <h4>{service.title}</h4>
-                    <p>{service.subtitle}</p>
-                </div>
-                ))):
-                <div class="spinner-grow text-info" role="status">
-                <span class="visually-hidden">Loading...</span>
-            </div>}
-                
+                {serviceData.length > 0 ? (
+                    serviceData.map((service, index) => (
+                        <div key={index} style={{ background: service.bg }}>
+                            <h2>{service.icon}</h2>
+                            <h4>{service.title}</h4>
+                            <p>{service.subtitle}</p>
+                        </div>
+                    ))) :
+                    <div class="spinner-grow text-info" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>}
             </div>
+
+            {/* -----------------BIG DISCOUNT------------------ */}
+
+            <div className='big-dis'>
+                <div>
+                    <h2>Big Discount</h2>
+                </div>
+
+                <div className='container'>
+                    <div className='row'>
+                        {discoutProducts.map((Discount, index) => (
+                            <div key={index} className='col-md-4 my-3'>
+                                <div className='card'>
+                                    <p>{Discount.discount}% Off</p>
+                                    <img src={Discount.imgUrl} alt='img' />
+                                    <p>{Discount.productName}</p>
+                                    <section>
+                                        <LiaStarSolid style={{ color: 'rgb(241 194 74)' }} />
+                                        <LiaStarSolid style={{ color: 'rgb(241 194 74)' }} />
+                                        <LiaStarSolid style={{ color: 'rgb(241 194 74)' }} />
+                                        <LiaStarSolid style={{ color: 'rgb(241 194 74)' }} />
+                                        <LiaStarSolid style={{ color: 'rgb(241 194 74)' }} />
+                                    </section>
+                                    <h2>${Discount.price}</h2>
+                                    <button><Plus /></button>
+                                </div>
+                            </div>))}
+                    </div>
+                </div>
+            </div>
+
+            {/* ------------------------------------NEW ARRIVALS---------------------------------------- */}
+            <div className='new-arr'>
+                <div>
+                    <h2>New Arrivals</h2>
+                </div>
+
+                <div className='container'>
+                    <div className='row'>
+                        {products.map((Discount, index) => (
+                            <div key={index} className='col-md-4 my-3'>
+                                <div className='card'>
+                                    <img src={Discount.imgUrl} alt='img' />
+                                    <p>{Discount.productName}</p>
+                                    <section>
+                                        <LiaStarSolid style={{ color: 'rgb(241 194 74)' }} />
+                                        <LiaStarSolid style={{ color: 'rgb(241 194 74)' }} />
+                                        <LiaStarSolid style={{ color: 'rgb(241 194 74)' }} />
+                                        <LiaStarSolid style={{ color: 'rgb(241 194 74)' }} />
+                                        <LiaStarSolid style={{ color: 'rgb(241 194 74)' }} />
+                                    </section>
+                                    <h2>${Discount.price}</h2>
+                                    <button><Plus /></button>
+                                </div>
+                            </div>))}
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
 export default Home
-
-// import { PlusCircle, ShoppingBag, Star } from "react-feather"
-// import hero from "../Assets/hero-img.png"
-
-// import productImg01 from '../Assets/double-sofa-01.png';
-// import { MdOutlineWallet } from "react-icons/md";
-// import { FaCar } from "react-icons/fa";
-// import { RiSecurePaymentFill } from "react-icons/ri";
-// import { FaHeadphones } from "react-icons/fa";
-// import  "./Home.css"
-// import { useState } from "react";
-
-// const Home =({count})=>{
-//     const [shopping]=useState(
-//         {
-//         heading:"50% Off For Your\nFirst Shopping",
-//         para:"Lorem ipsum dolor sit amet,consectetur adipiscing elit.\n Quit lobortis consequent eu,quam etiam at quis ut\n convallis",
-
-//         }
-//     )
-//     const [shopping2]=useState(
-//         {
-//         heading:"Free Shipping",
-//         para:"Lorem ipsum dolor sit amet",
-//         safe:"Safe Payment",
-//         secure:"Secure Payment",
-//         guarantee :"Back Guarantee"
-        
-
-//         }
-//     )
-
-//     const [Discount]=useState({
-//         para:"Stone and Beam Westview",
-//         rate:"193"
-      
-
-//     })
-//     // break
-//     const headingLines=shopping.heading.split("\n")
-//     const paraline=shopping.para.split("\n")
-//     const starsArray = new Array(5).fill(null);
-
-//     function click(){
-//         alert("hello")
-//     }
-
-
-    
-
-   
-//     return(
-        
-//         <div>
-//          <div className="container"> 
-//             <div className="row" >
-                
-//                 <div className="col-md-6" style={{marginTop:"80px"}} >
-//                     <h2>
-//                      {headingLines.map((line, index) => (
-//                 <span key={index}>
-//                   {line}
-//                   {index !== headingLines.length - 1 && <br />}
-//                 </span>
-//               ))}</h2>
-//                     <p>
-//                         {
-//                             paraline.map((para,index)=>(
-//                                 <span>{para} {index !== paraline.length - 1 && <br />}</span>
-//                             ))
-//                         }
-//                     </p>
-//                     <button>Visit collections</button>
-
-//                 </div>
-//                 <div className="col-md-6">
-//                     <img src={hero} alt="Hero Image"/>
-//                  </div>
-                
-           
-//             </div>
-                
-            
-//             </div>
-
-//             {/* cards */}
-//             <div className="container" >
-//                 <div className="row">
-//                         <div className="col-md-3">
-//                             <div className="card" style={{backgroundColor:"yellow",width:"265px"}}>
-//                                 <h1><FaCar /></h1>
-//                                 <h2>{shopping2.heading}</h2>
-//                                  <p>{shopping2.para}</p>
-//                             </div>
-                       
-//                         </div>
-
-//                         <div className="col-md-3">
-//                             <div className="card" style={{backgroundColor:"skyblue",width:"265px"}}>
-//                                 <h1><MdOutlineWallet/></h1>
-//                                 <h2>{shopping2.safe}</h2>
-//                                 <p>{shopping2.para}</p>
-
-//                             </div>
-                       
-//                         </div>
-
-//                         <div className="col-md-3">
-//                             <div className="card" style={{backgroundColor:"lightgreen", width:"295px"}}>
-//                                 <h1><RiSecurePaymentFill /></h1>
-//                                 <h2>{shopping2.secure}</h2>
-//                                 <p>{shopping2.para}</p>
-
-//                             </div>
-                       
-//                         </div>
-
-//                         <div className="col-md-3">
-//                             <div className="card" style={{backgroundColor:"purple",width:"295px",marginLeft:"30px"}}>
-//                                  <h1><FaHeadphones /></h1>
-//                                  <h2>{shopping2.guarantee}</h2>
-//                                 <p>{shopping2.para}</p>
-
-//                             </div>
-                       
-//                         </div>
-                    
-
-//                 </div>
-
-//             </div>
-
-
-//             {/* Bigg Discount */}
-//             <div className="container" >
-//                 <div className="row">
-//                     <div className="col-md-4">
-//                         <div className="card" style={{width:"200px"}}>
-//                             <button className="position-absolute top-0 start-0 m-3" 
-//                             style={{backgroundColor:"violet",borderRadius:"25px",width:"90px",marginRight:"50px"}}>30%</button>
-//                             <img src={productImg01}/>
-//                             <p>{Discount.para}</p>
-//                             <div className="stars-container">
-//                                 {starsArray.map((user, index) => (
-//                                 <Star key={index} fill="gold" /> 
-//                                 ))}
-//                             </div>
-//                             <h2>${Discount.rate}</h2>
-//                             <h2 onClick={()=>{click()}} className="position-absolute bottom-0 end-0 m-3" 
-//                             style={{borderRadius:"50px",width:"25px",height:"25px"}}><PlusCircle/> </h2>
-
-
-
-//                         </div>
-//                     </div>
-//                     <div className="col-md-4">
-//                     <div className="card" style={{width:"200px"}}>
-//                             <button className="position-absolute top-0 start-0 m-3" 
-//                             style={{backgroundColor:"violet",borderRadius:"25px",width:"90px",marginRight:"50px"}}>30%</button>
-//                             <img src={productImg01}/>
-//                             <p>{Discount.para}</p>
-//                             <div className="stars-container">
-//                                 {starsArray.map((user, index) => (
-//                                 <Star key={index} fill="gold" /> 
-//                                 ))}
-//                             </div>
-//                             <h2>${Discount.rate}</h2>
-//                             <h2 onClick={()=>{click()}} className="position-absolute bottom-0 end-0 m-3" 
-//                             style={{borderRadius:"50px",width:"25px",height:"25px"}}><PlusCircle/> </h2>
-
-
-
-//                         </div>
-//                     </div>
-//                     <div className="col-md-4">
-//                     <div className="card" style={{width:"200px"}}>
-//                             <button className="position-absolute top-0 start-0 m-3" 
-//                             style={{backgroundColor:"violet",borderRadius:"25px",width:"90px",marginRight:"50px"}}>30%</button>
-//                             <img src={productImg01}/>
-//                             <p>{Discount.para}</p>
-//                             <div className="stars-container">
-//                                 {starsArray.map((user, index) => (
-//                                 <Star key={index} fill="gold" /> 
-//                                 ))}
-//                             </div>
-//                             <h2>${Discount.rate}</h2>
-//                             <h2 onClick={()=>{click()}} className="position-absolute bottom-0 end-0 m-3" 
-//                             style={{borderRadius:"50px",width:"25px",height:"25px"}}><PlusCircle/> </h2>
-
-
-
-//                         </div>
-//                     </div>
-
-//                 </div>
-
-//                 <div className="row">
-//                     <div className="col-md-4">
-//                         <div className="card" style={{width:"200px"}}>
-//                             <button className="position-absolute top-0 start-0 m-3" 
-//                             style={{backgroundColor:"violet",borderRadius:"25px",width:"90px",marginRight:"50px"}}>30%</button>
-//                             <img src={productImg01}/>
-//                             <p>{Discount.para}</p>
-//                             <div className="stars-container">
-//                                 {starsArray.map((user, index) => (
-//                                 <Star key={index} fill="gold" /> 
-//                                 ))}
-//                             </div>
-//                             <h2>${Discount.rate}</h2>
-//                             <h2 onClick={()=>{click()}} className="position-absolute bottom-0 end-0 m-3" 
-//                             style={{borderRadius:"50px",width:"25px",height:"25px"}}><PlusCircle/> </h2>
-
-
-
-//                         </div>
-//                     </div>
-//                     <div className="col-md-4">
-//                     <div className="card" style={{width:"200px"}}>
-//                             <button className="position-absolute top-0 start-0 m-3" 
-//                             style={{backgroundColor:"violet",borderRadius:"25px",width:"90px",marginRight:"50px"}}>30%</button>
-//                             <img src={productImg01}/>
-//                             <p>{Discount.para}</p>
-//                             <div className="stars-container">
-//                                 {starsArray.map((user, index) => (
-//                                 <Star key={index} fill="gold" /> 
-//                                 ))}
-//                             </div>
-//                             <h2>${Discount.rate}</h2>
-//                             <h2 onClick={()=>{click()}} className="position-absolute bottom-0 end-0 m-3" 
-//                             style={{borderRadius:"50px",width:"25px",height:"25px"}}><PlusCircle/> </h2>
-
-
-
-//                         </div>
-//                     </div>
-//                     <div className="col-md-4">
-//                     <div className="card" style={{width:"200px"}}>
-//                             <button className="position-absolute top-0 start-0 m-3" 
-//                             style={{backgroundColor:"violet",borderRadius:"25px",width:"90px",marginRight:"50px"}}>30%</button>
-//                             <img src={productImg01}/>
-//                             <p>{Discount.para}</p>
-//                             <div className="stars-container">
-//                                 {starsArray.map((user, index) => (
-//                                 <Star key={index} fill="gold" /> 
-//                                 ))}
-//                             </div>
-//                             <h2>${Discount.rate}</h2>
-//                             <h2 onClick={()=>{click()}} className="position-absolute bottom-0 end-0 m-3" 
-//                             style={{borderRadius:"50px",width:"25px",height:"25px"}}><PlusCircle/> </h2>
-
-
-
-//                         </div>
-//                     </div>
-
-//                 </div>
-
-//             </div>
-
-            
-  
-       
-//         </div>
-//     )
-      
-// }
-// export default Home
