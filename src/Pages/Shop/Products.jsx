32c7a956010c7ff583c59978e3b303e0dc858bc8
karthee21 <mutyalakarthik6@gmail.com>
2024-04-13 00:16:58 +0530

    Creating Shop page

--- src/Pages/Shop/Products.jsx
+++ src/Pages/Shop/Products.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import BestSales from '../Home/BestSales'
-
+import './table.jpg'
+import "./Products.css"
 const Products = () => {
     return (
         <>
             <div className='banner'>
                 <h2>Products</h2>
+            </div>
+            <div>
+                <form className='search'>
+                    <select >
+                        <option value="main" disabled>Filter By Category</option>
+                        <option value="mobils">Mobils</option>
+                        <option value="chairs">Chairs</option>
+                        <option value="Sofas">Sofas</option>
+                        <option value="wireless">Wireless</option>
+                    </select>
+                    <input type="search" placeholder='Search...'/>
+                </form>
             </div>
             <BestSales />
         </>
